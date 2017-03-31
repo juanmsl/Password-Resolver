ba44f6f440b9fc45ee01e926102dc5801f5afd3c
Juan Manuel Sánchez Lozano <juanmsl_pk@hotmail.com>
2017-03-30 21:58:37 -0500

    Getting password from any hast function added

--- src/controller/Machine.java
+++ src/controller/Machine.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package controller;
 
 public class Machine {
@@ -38,30 +37,3 @@ public class Machine {
 		this.server.removeMachine(this.port);
 	}
 }
-=======
-package controller;
-
-public class Machine {
-	public static int MACHINE_CONSECUTIVE = 0;
-	
-	private int id;
-	private int port;
-	private MachineConection machineThread;
-	
-	public Machine(int port) {
-		this.id = ++Machine.MACHINE_CONSECUTIVE;
-		this.port = port;
-		this.machineThread = new MachineConection(this.port);
-		this.machineThread.start();
-	}
-	
-	@Override
-	public String toString() {
-		return String.format("Machine %s: [Port: %s]", this.id, this.port);
-	}
-	
-	public void sendInt(int n) {
-		this.machineThread.writeInt(n);
-	}
-}
->>>>>>> origin/master
--- src/controller/MachineConection.java
+++ src/controller/MachineConection.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package controller;
 
 import java.io.DataInputStream;
@@ -73,57 +72,3 @@ public class MachineConection extends Thread {
 		}
 	}
 }
-=======
-package controller;
-
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.net.ServerSocket;
-import java.net.Socket;
-
-public class MachineConection extends Thread {
-	private ServerSocket serverSocket;
-	private DataInputStream in;
-	private DataOutputStream out;
-	private int port;
-	
-	public MachineConection(int port) {
-		try {
-			this.in = null;
-			this.out = null;
-			this.port = port;
-			System.out.println("Enable conection on port " + port + "...");
-			this.serverSocket = new ServerSocket(port);
-			System.out.println("Conection enabled on port " + port);
-		}
-		catch (IOException event) {
-			System.out.println("Error: [" + event.getMessage() + "]");
-		}
-	}
-	
-	@Override
-	public void run() {
-		try {
-			System.out.println("Waiting for the machine that will use the port " + this.port + "...");
-			Socket socket = this.serverSocket.accept();
-			System.out.println("...A machine was conected on port " + this.port);
-			this.in = new DataInputStream(socket.getInputStream());
-			this.out = new DataOutputStream(socket.getOutputStream());
-		}
-		catch (IOException event) {
-			System.out.println("Error: [" + event.getMessage() + "]");
-			event.printStackTrace();
-		}
-	}
-	
-	public void writeInt(int n) {
-		try {
-			this.out.writeInt(n);
-		}
-		catch (IOException event) {
-			System.out.println("Error: [" + event.getMessage() + "]");
-		}
-	}
-}
->>>>>>> origin/master
